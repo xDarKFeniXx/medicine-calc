0ae8b006eae9c6924904174268b51c896ad08e17
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-11-03 18:12:16 +0300

    fix favico

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.png'));
 
 //здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
